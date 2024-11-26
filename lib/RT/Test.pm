bestpractical/rt

Add support for writing REST2 test results to open API schema
    perl devel/tools/rt-json-to-open-api

lib/RT/Test.pm
@@ -1852,6 +1852,11 @@ sub done_testing {
         $cleanup->no_warnings_ok;
     }
 
+    # Check the environment variable before writing the requests log
+    if ( $ENV{WRITE_REQUESTS_LOG} ) {
+        RT::Test::REST2->export_requests_to_yaml();
+    }
+
     $builder->done_testing(@_);
 }
 

ParagShah97/rt6_AIEnabled

lib/RT/Test.pm
@@ -1852,6 +1852,11 @@ sub done_testing {
         $cleanup->no_warnings_ok;
     }
 
+    # Check the environment variable before writing the requests log
+    if ( $ENV{EXPORT_OPENAPI_YAML} ) {
+        RT::Test::REST2->export_requests_to_yaml();
+    }
+
     $builder->done_testing(@_);
 }
 